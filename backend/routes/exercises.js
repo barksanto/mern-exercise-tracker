82fd672dc0581624d98ea0f39db22a8417b5d4a6
Barkley Santo <barkley.santo@gmail.com>
2021-02-21 13:56:57 -0500

    exercises router

---
+++ backend/routes/exercises.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+let Exercise = require('../models/exercise.model');
+
+router.route('/').get((req, res) =>{
+  Exercise.find()
+  .then(exercises => res.json(exercises))
+  .catch(er => res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post((req, res) => {
+  const username = req.body.username;
+  const description = req.body.description;
+  const duration = Number(req.body.duration);
+  const date = Date.parse(req.bnody.date);
+  
+})
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,10 +2,20 @@ const router = require('express').Router();
 
 let User = require('../models.user.model');
 
+// the first endpoint that handles http get requests
 router.route('/').get((req, res) => {
   User.find()
-  /TouchEvent(users = res.json(users))
-  .catch(err => res.status(400).json('Error: ' + err));
+    .then(users = res.json(users))
+    .catch(err => res.status(400).json('Error: ' + err));
 })
 
-router.route('/add')
+router.route('/add').post((req, res) => {
+  const username = req.body.username;
+  const newUser = new User({ username });
+
+  newUser.save()
+    .then(() => res.json('User Added!'))
+    .catch(err => res.status(400).json('Error: ' + err));
+})
+
+module.exports = router;
