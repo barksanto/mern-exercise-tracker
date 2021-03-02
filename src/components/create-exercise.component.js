9fedf00e05ef9e48cad4b404eda870355965d8cf
Barkley Santo <barkley.santo@gmail.com>
2021-03-01 19:49:53 -0500

    create excercises should be able to add exercises to the database

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+// this will aloow us to add exercises to the database
 export default class CreateExercise extends Component {
   render() {
     return (
