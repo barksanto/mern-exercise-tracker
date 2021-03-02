207a0b437e5622b9c25b465ba3dabd8805db52c0
Barkley Santo <barkley.santo@gmail.com>
2021-03-01 19:56:09 -0500

    added constructor class with state

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 // this will aloow us to add exercises to the database
+
 export default class CreateExercise extends Component {
+  constructor(props) {
+    //In js classes need to always call define a super when defining the consructor of a subclass
+    super(props);
+
+    this.state = {
+      username: '',
+      description: '',
+      duration: 0,
+      date: new Date(),
+      users: []
+    }
+  }
+
   render() {
     return (
       <div>
