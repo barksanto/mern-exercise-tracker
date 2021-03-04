be7b6bd1bae53158bbd183db52700fd38d053e61
Barkley Santo <barkley.santo@gmail.com>
2021-03-04 10:42:49 -0500

    code will run before page is rendered and add the list of exercises to the state

--- src/components/exercises-list.component.js
+++ src/components/exercises-list.component.js
@@ -1,6 +1,27 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export default class ExercisesList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.deleteExercise = this.deleteExercise.bind(this);
+
+    this.state = { exercises: [] }
+  }
+
+  // code will ru nbefore page is rendered and add the list of exercises to the state
+  componentDidMount() {
+    axios.get('http://localhost:5000/exercises/')
+      .then(response => {
+        this.setState({ exercises: response.data });
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
   render() {
     return (
       <div>
