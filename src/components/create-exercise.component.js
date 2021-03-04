d5242c388a083f44123745aeb05740e6942e4902
Barkley Santo <barkley.santo@gmail.com>
2021-03-04 10:36:17 -0500

    first selected option in users dropdown is index 0 of the array

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -36,7 +36,7 @@ export default class CreateExercise extends Component {
         if (response.data.length > 0) {
           this.setState({
             users: response.data.map(user => user.username),
-            username: 'test user'
+            username: response.data[0].username
           })
         }
       })
